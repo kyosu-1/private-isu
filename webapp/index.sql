1c2c2919a37717a2f59935fc98a33ef23fd7a211
kyosu-1 <sho013039@gmail.com>
2024-12-01 09:16:21 +0900

    del index

--- webapp/index.sql
+++ webapp/index.sql
@@ -1,5 +1,4 @@
 -- 直接適用するが、ここに記載しておく
 ALTER TABLE comments ADD INDEX idx_post_id_created_at (post_id, created_at);
 ALTER TABLE posts ADD INDEX idx_created_at (created_at);
-ALTER TABLE posts ADD INDEX idx_user_id (user_id);
 ALTER TABLE comments ADD INDEX idx_user_id (user_id);
