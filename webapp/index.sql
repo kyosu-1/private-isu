c82a96914076806a0eee9ba8928f6d2fe2ae95ff
kyosu-1 <sho013039@gmail.com>
2024-12-04 06:17:32 +0900

    add user_id created_at index to posts

--- webapp/index.sql
+++ webapp/index.sql
@@ -2,3 +2,4 @@
 ALTER TABLE comments ADD INDEX idx_post_id_created_at (post_id, created_at);
 ALTER TABLE posts ADD INDEX idx_created_at (created_at);
 ALTER TABLE comments ADD INDEX idx_user_id (user_id);
+ALTER TABLE posts ADD INDEX idx_user_id_created_at (user_id, created_at DESC);
